06e1aa5c704d18bbc369b1d9bef917c52fc6f605
npne269 <npne269@gmail.com>
2019-02-17 23:51:48 +0545

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-$email=$_POST['email1']; // Fetching Values from URL.
-$password= sha1($_POST['password1']);
-
-if($email == "user" and $password == "pass")
-    echo "OK";
-else
-    echo "KO";
